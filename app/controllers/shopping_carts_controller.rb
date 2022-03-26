f52558f6463e48d18276cf1a6b366ca8fb96f035
綾乃 <>
2022-03-26 23:52:42 +0900

    createアクション

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -8,8 +8,20 @@ class ShoppingCartsController < ApplicationController
     @cart = ShoppingCart.find(user_id: current_user)
   end
   
+  def create
+    @product = Product.find(product_params[:product_id])
+    @user_cart.add(@product, product_params[:price].to_i, product_params[:quantity].to_i)
+    redirect_to cart_users_path
+  end
+
+
   private
-    def set_cart
-      @user_cart = ShoppingCart.set_user_cart(current_user)
-    end
+
+  def product_params
+    params.permit(:product_id, :price, :quantity)
+  end
+
+  def set_cart
+    @user_cart = ShoppingCart.set_user_cart(current_user)
+  end
 end
