f354cc86897e3f49b3fb13b4d17848b7eef97a07
綾乃 <>
2022-03-26 23:50:35 +0900

    showアクション

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -4,6 +4,10 @@ class ShoppingCartsController < ApplicationController
     @user_cart_items = ShoppingCartItem.user_cart_items(@user_cart)
   end
 
+  def show
+    @cart = ShoppingCart.find(user_id: current_user)
+  end
+  
   private
     def set_cart
       @user_cart = ShoppingCart.set_user_cart(current_user)
